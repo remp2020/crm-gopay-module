bcab994a2c3fd21179a40e5eb52e51925f325885
Tibor Mikoczy <mikoczy@efabrica.sk>
2021-04-08 15:57:48 +0200

    Fix `isPendingState` should handle null state
    When gopay returns an error, it's possible that
    `$data[’state’]` parameter is not set.
    This was causing issues on `isPendingState` check
    because string status was expected and missing.
    
    remp2020/crm-gopay-module#10

--- src/gateways/BaseGoPay.php
+++ src/gateways/BaseGoPay.php
@@ -131,11 +131,12 @@ abstract class BaseGoPay extends GatewayAbstract
         }
 
         // return null state [form] - wait for notification
-        if ($this->isPendingState($data['state'])) {
+        $state = $data['state'] ?? null;
+        if ($this->isPendingState($state)) {
             return null;
         }
 
-        return $data['state'] == self::STATE_PAID;
+        return $state == self::STATE_PAID;
     }
 
     public function notification(ActiveRow $payment, string $id, ?string $parentId = null): bool
@@ -388,9 +389,9 @@ abstract class BaseGoPay extends GatewayAbstract
         return $this->applicationConfig->get('site_title');
     }
 
-    protected function isPendingState(string $state): bool
+    protected function isPendingState(?string $state): bool
     {
-        return in_array($state, [self::STATE_CREATED, self::STATE_PAYMENT_METHOD_CHOSEN]);
+        return in_array($state, [self::STATE_CREATED, self::STATE_PAYMENT_METHOD_CHOSEN], true);
     }
 
     protected function getError(): ?array
