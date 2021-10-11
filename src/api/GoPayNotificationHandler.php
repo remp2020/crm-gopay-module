0d82b4c9d0d87f890e87383e8b5fd02df529d381
Dominik Skerhak <dominik.skerhak@gmail.com>
2021-10-11 08:29:42 +0200

    Nette v3.1 - IResponse is deprecated; use Response (Nette\Application)
    remp/crm#1979

--- src/api/GoPayNotificationHandler.php
+++ src/api/GoPayNotificationHandler.php
@@ -48,7 +48,7 @@ class GoPayNotificationHandler extends ApiHandler
 
     /**
      * @param ApiAuthorizationInterface $authorization
-     * @return \Nette\Application\IResponse
+     * @return \Nette\Application\Response
      */
     public function handle(ApiAuthorizationInterface $authorization)
     {
